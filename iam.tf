aa017bc2b26dec9c685b7b1ce8646b3d756217fc
Sam Elliott <soe1@uakron.edu>
2025-09-26 22:53:25 -0400

    HAD TO CHECK THE FORMATTING AGAIN HOOOOOO

--- iam.tf
+++ iam.tf
@@ -39,7 +39,7 @@ resource "aws_iam_role" "this" {
 data "aws_iam_policy_document" "deploy" {
   statement {
     effect = "Allow"
-     actions = [
+    actions = [
       "ecr:*",
       "s3:GetObject",
       "s3:PutObject",
